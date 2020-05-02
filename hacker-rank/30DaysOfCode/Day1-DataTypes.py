669d2c7783462f74bce4a989fccc09ac68b31bc6
baskpann <baskpann@gmail.com>
2020-05-02 23:27:20 +0530

    Day1: Data Types

---
+++ hacker-rank/30DaysOfCode/Day1-DataTypes.py
@@ -0,0 +1,20 @@
+i = 4
+d = 4.0
+s = HackerRank 
+# Declare second integer, double, and String variables.
+x = 0
+y = 0.0
+z = 
+# Read and save an integer, double, and String to your variables.
+x = int(input())
+y = float(input())
+z = input()
+# Print the sum of both integer variables on a new line.
+print("%d" % (i + x))
+
+# Print the sum of the double variables on a new line.
+print("%-8.1f" % (d + y))
+
+# Concatenate and print the String variables on a new line
+# The s variable above should be printed first.
+print(s + z)
