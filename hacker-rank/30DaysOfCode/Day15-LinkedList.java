f49f1b4b0714c1c100926b5e386dadbd722731af
baskpann <baskpann@gmail.com>
2020-05-03 00:42:40 +0530

    Day15: Linked List

---
+++ hacker-rank/30DaysOfCode/Day15-LinkedList.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+class Node {
+	int data;
+	Node next;
+	Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+class Solution {
+
+    public static  Node insert(Node head,int data) {
+        //Complete this method
+        if(head == null) return new Node(data);
+        Node tmp = new Node(data);
+        Node p = head;
+        while(p.next != null)
+            p = p.next;
+        p.next = tmp;
+        return head;
+    }
+
+	public static void display(Node head) {
+        Node start = head;
+        while(start != null) {
+            System.out.print(start.data + " ");
+            start = start.next;
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        Node head = null;
+        int N = sc.nextInt();
+
+        while(N-- > 0) {
+            int ele = sc.nextInt();
+            head = insert(head,ele);
+        }
+        display(head);
+        sc.close();
+    }
+}
