5155df0ed151387c00e978408ac5a6af9ac681af
baskpann <baskpann@gmail.com>
2020-05-06 13:48:38 +0530

    LeetCode MayChallenge Week1_Day5: Majoriy Element

---
+++ leet-code/MayChallenge/MajorityElement.java
@@ -0,0 +1,20 @@
+
+class Solution {
+    private int count(int[] a, int k){
+        int cnt = 0;
+        for(int i = 0; i < a.length; i++)
+            if(a[i] == k)
+                cnt++;
+        return cnt;
+    }
+    public int majorityElement(int[] nums) {
+        int i, cnt = 0, maxCnt = nums.length/2;
+        for(i = 0; i < nums.length; i++){
+            cnt = count(nums, nums[i]);
+            if(cnt > maxCnt){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}
